411fd1b910c21b0387a996e8dd2a80f4cb0be49a
gijoona <devilkij77@gmail.com>
2021-09-01 18:54:17 +0900

    fileupload path modify

--- src/conf/multer.options.ts
+++ src/conf/multer.options.ts
@@ -5,7 +5,7 @@ import uuidRandom from "./uuidRandom";
 
 export const multerOptions = {
   fileFilter: (request, file, callback) => {
-    if (file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
+    if (file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
       // 이미지 형식은 jpg, jpeg, png만 허용합니다.
       callback(null, true);
     } else {
@@ -16,10 +16,10 @@ export const multerOptions = {
 
   storage: diskStorage({
     destination: (request, file, callback) => {
-      const uploadPath: string = 'public';
+      const uploadPath: string = 'src/assets/images';
 
       if (!existsSync(uploadPath)) {
-        // public 폴더가 존재하지 않을시, 생성합니다.
+        // src/assets/images 폴더가 존재하지 않을시, 생성합니다.
         mkdirSync(uploadPath);
       }
 
@@ -34,7 +34,7 @@ export const multerOptions = {
 
 export const createImageURL = (file): string => {
   
-  // 파일이 저장되는 경로: 서버주소/public 폴더
+  // 파일이 저장되는 경로: 서버주소/src/assets/images 폴더
   // 위의 조건에 따라 파일의 경로를 생성해줍니다.
-  return `/public/${file.filename}`;
+  return `/src/assets/images/${file.filename}`;
 }
